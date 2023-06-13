51b4c94fef4bc7acff91c19594bf21dbcea3e858
minwung <alsdnd336@naver.com>
2023-06-13 10:24:43 +0900

    Add name

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyAppState extends State<MyApp> {g
   @override
   Widget build(BuildContext context) {
     return Scaffold(
